5a129d3b2d40572580f58baf991f2434808b5a0d
Sheikh Hamim Mahmud <hamim.mahmud11@gmail.com>
2024-01-02 09:50:43 +0600

    first commit

---
+++ 4A/4A/4A/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number%2==0 && number > 2)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+            Console.ReadKey();
+        }
+    }
+}
