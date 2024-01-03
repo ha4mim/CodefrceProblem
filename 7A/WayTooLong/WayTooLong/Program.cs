5b29760b3a35dbf7c55dc9bea42e03bf7c0c64c7
Sheikh Hamim Mahmud <hamim.mahmud11@gmail.com>
2024-01-04 00:00:20 +0600

    commit for push

---
+++ 7A/WayTooLong/WayTooLong/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WayTooLong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            for(int i=0; i<number; i++)
+            {
+                string word = Console.ReadLine();
+                if (word.Length > 10)
+                {
+                    Console.WriteLine($"{word[0]}{word.Length - 2}{word[word.Length - 1]}");
+                }
+                else
+                {
+                    Console.WriteLine(word);
+                }
+            }
+            
+        }
+    }
+}
